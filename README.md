098a2db4efac742ff2e3384c192aae9cb48ae783
Lidan Hifi <lidanh@wix.com>
2016-04-10 00:55:50 +0300

    analyze funnel

--- README.md
+++ README.md
@@ -1,8 +1,13 @@
 # Israel Government Data Exploration
 This is an assignment in the data scientist course at BGU university.
 
-In this assignment I'll explore vegetables and fruits prices, based on pricing dataset which provided by the **ministry of agriculture & rural development**.
+In this assignment I'll analyze funnel data of a SaaS product which enables users to send payment requests and get paid online.
 
-The main idea is to gather data from this dataset (xls file), and show some basic insights about this data, such as basic graphs and correlations.
+The main idea is to gather data from this dataset (csv file), and show some basic insights about this data, such as basic graphs and correlations.
 
 Work in progress, more info will be available soon.
+
+## Funnel Steps
+1. Create payment request
+2. Send payment request
+3. User of user (aka consumer) pay online or offline
---
+++ funnel_analysis.R
@@ -0,0 +1,92 @@
+setwd("~/Projects/bgu/funnel_analysis")
+
+# Download dataset
+datasetFile <- "funnel_data.csv"
+# datasetUrl <- "TODO"
+
+if (file.exists(datasetFile)) {
+  file.remove(datasetFile)
+}
+
+# download.file(datasetUrl, destfile = datasetFile, method = "curl")
+
+# === Load Dataset ===
+funnel <- read.csv(datasetFile)
+
+# === Basic Exploration ===
+summary(funnel)
+head(funnel)
+
+# === Graphs ===
+library(ggplot2)
+library(reshape2)
+
+# 1. Basic funnel analysis
+boxplot(funnel$new_items, 
+        funnel$total_sent, 
+        funnel$payment_started, 
+        funnel$payment_finished, 
+        funnel$paid_offline, 
+        main="Funnel Steps Analysis",
+        ylab ="Total", xlab ="Funnel Steps",
+        names = c("New Items", "Total Sent", "Payment Started", "Payment Finished", "Paid Offline"),
+        col = c("#459fed", "#32babc", "#32b76c", "#a0138e", "#faa030"))
+
+
+
+# 2. Visualizing Funnel (group by date)
+visualize_funnel <- function(title, steps) {
+  total_by_day <- aggregate(steps, by = list(Date = funnel$date), FUN = sum)
+  melted_totals <- melt(total_by_day, id = "Date", variable.name = "Step")
+  ggplot(melted_totals, aes(x = Date, y = value, colour=Step, group = 1)) + 
+    geom_smooth() +
+    geom_line() +
+    ylab(label="Total") + 
+    xlab("Date") +
+    ggtitle(title) +
+    theme(
+      axis.text.x=element_blank(), 
+      plot.title = element_text(size=24, face="bold"))
+}
+
+# User end: new + sent
+visualize_funnel("Users Funnel", list(New = funnel$new_items, Sent = funnel$total_sent))
+
+# User of user end: start pay + finish pay
+visualize_funnel("Users of Users Funnel", list(Start_Payment = funnel$payment_started, Finish_Payment = funnel$payment_finished))
+
+
+
+# 3. Funnel Steps Correlations
+library(corrplot)
+M <- cor(subset(funnel, select = c("new_items", "total_sent", "payment_started", "payment_finished", "paid_offline")))
+corrplot(M, method = "ellipse")
+
+
+
+# 4. New-sent correlation
+users_funnel_corr <- cor(funnel$new_items, funnel$total_sent)
+ggplot(funnel, aes(x = new_items, y = total_sent, group = 1)) +
+  geom_point(shape=16, color = "#0072B2") +
+  geom_smooth(method=lm, se=FALSE, color="red", formula = y ~ x) +
+  ylab(label="Sent Items") + 
+  xlab("New Items") +
+  ggtitle(paste("Create vs. Send Correlation: ", users_funnel_corr)) +
+  theme(plot.title = element_text(size=24, face="bold"))
+
+
+
+# 5. Histogram of starting payment by hour
+avg_by_hour <- function(title, steps) {
+  avg_by_hour_value <- aggregate(steps, by = list(Hour = funnel$hour), FUN = mean)
+  melted_totals <- melt(avg_by_hour_value, id = "Hour", variable.name = "Step", stat="identity")
+  ggplot(melted_totals, aes(x = Hour, y = value)) + 
+    geom_bar(stat="identity", fill="#733ca6") +
+    ylab(label="Avg.") + 
+    xlab("Hour") +
+    ggtitle(title) +
+    stat_smooth(color = "#faa030", se=FALSE) + 
+    theme(plot.title = element_text(size=24, face="bold"))
+}
+
+avg_by_hour("Avg. Payments by Hour", funnel$payment_started)
